85fde24e22f4694f9cf65cd46bbfe4c3291a6ccb
Subham gaurav <shubham.gaurav.1994@gmail.com>
2016-02-18 01:31:31 +0530

    final app

--- config/database.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by subham_1994 on १३-०२-२०१६.
- */
-(function() {
-    module.exports = {
-        url: 'mongodb://subham_1994:subham_1994@ds011298.mongolab.com:11298/nodeauthdemo'
-    };
-}());
