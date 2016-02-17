48120cb061cb4ff7fd5ff86c76c47127be6a0172
Subham gaurav <shubham.gaurav.1994@gmail.com>
2016-02-17 15:23:50 +0530

    updated facebook auth for heroku

--- config/auth.js
+++ config/auth.js
@@ -7,12 +7,12 @@
         'facebookAuth': {
             'clientId': '435757103282039',
             'clientSecret': 'b534c42ca49e7fe225d9219b6fbb7572',
-            'callbackUrl': 'http://localhost:3000/auth/facebook/callback'
+            'callbackUrl': 'https://node-socialauth-live.herokuapp.com/auth/facebook/callback'
         },
         'googleAuth': {
             'clientId': '392367740117-5b726hhseee72854n91i6hvuoromql65.apps.googleusercontent.com',
             'clientSecret': 'LZThR1yinI2Uhiw6ybM07gyf',
-            'callbackUrl': 'http://localhost:3000/auth/google/callback'
+            'callbackUrl': 'https://node-socialauth-live.herokuapp.com/auth/google/callback'
         }
     };
 }());
